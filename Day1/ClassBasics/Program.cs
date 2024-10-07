5591369e421160a751942bebe5823b7eb80aef31
pradipp4799 <pradipp4799@gmail.com>
2024-10-07 11:46:30 +0530

    add day1

---
+++ Day1/ClassBasics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBasics
+{
+    class Program
+    {
+        static void Main()
+        {
+            //System.Console.WriteLine("HelloWorld");
+            //Console.WriteLine("Hello");
+            //Console.ReadLine();
+            Class1 c = new Class1();
+            c.Display();
+            c.Display("Pradip");
+            Console.ReadLine() ;
+        }
+    }
+
+    public class Class1
+    {
+        int i = 10;
+        public void Display()
+        {
+            Console.WriteLine("Display called " + i);
+        }
+        //Method overloading 
+        public void Display(String s)
+        {
+            Console.WriteLine("Display called " + s);
+        }
+    }
+}
