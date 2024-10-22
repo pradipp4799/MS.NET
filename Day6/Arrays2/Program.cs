987be48a5efd8553c55ed575bb41a04752b4a4e5
pradipp4799 <pradipp4799@gmail.com>
2024-10-22 14:27:34 +0530

    add day6

---
+++ Day6/Arrays2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays2
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            int[] arr = new int[3];
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter the array element {0}",i);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            //Array.Clear(arr, 0, arr.Length);
+
+            //Array.Copy
+
+            //Array.ConstrainedCopy
+
+            //int []arr2 = (int[])Array.CreateInstance(typeof(int), arr.Length);
+            
+            //Array.Sort(arr);
+            
+            //Array.Reverse(arr);
+            
+            
+            Console.ReadLine();
+        }
+        static void Main2(string[] args)
+        {
+            //Store marks for 5 students , 3 subjects marks
+            int[,] arr = new int[3,2];
+
+            //Console.WriteLine(arr.Length);              //15
+            //Console.WriteLine(arr.GetLength(0));        //5
+            //Console.WriteLine(arr.GetLength(1));        //3
+            //Console.WriteLine(arr.Rank);                //2
+            //Console.WriteLine(arr.GetUpperBound(0));    //4  upper index of 5 
+            //Console.WriteLine(arr.GetLowerBound(0));    //0
+
+           for(int i = 0;i < arr.GetLength(0);i++)
+            {
+                for(int j = 0;j < arr.GetLength(1); j++)
+                {
+                    Console.WriteLine("Enter the arr[{0},{1}]:", i, j);
+                    arr[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.WriteLine("arr[{0},{1}] :{2}", i, j, arr[i,j]);
+                }
+            }
+            Console.ReadLine();
+        }
+        static void Main3(string[] args)
+        {
+            //Jagged
+            int[][] arr = new int[4][];
+            arr[0] = new int[4];
+            arr[1] = new int[3];
+            arr[2] = new int[2];
+            arr[3] = new int[3];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.Write("Enter value for subscript [{0},{1}]:", i, j);
+                    arr[i][j] = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.WriteLine("value of subscript arr[{0},{1}] :{2}", i, j, arr[i][j]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day6/GenericsExample/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsExample
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            IntegerStack o1 = new IntegerStack(3);
+            o1.Push(1);
+            o1.Push(2); 
+            o1.Push(3);
+            //o1.Push(4);
+            Console.WriteLine(o1.Pop());
+            Console.WriteLine(o1.Pop());
+            Console.WriteLine(o1.Pop());
+
+        }
+
+        static void Main2()
+        {
+            StringStack o2 = new StringStack(3);
+            o2.Push("A");
+            o2.Push("B");
+            o2.Push("C");
+
+            Console.WriteLine(o2.Pop());
+            Console.WriteLine(o2.Pop());
+            Console.WriteLine(o2.Pop());
+
+        }
+
+        static void Main()
+        {
+            MyStack<String> o2 = new MyStack<String>(3);
+            o2.Push("A");
+            o2.Push("B");
+            o2.Push("C");
+
+            Console.WriteLine(o2.Pop());
+            Console.WriteLine(o2.Pop());
+            Console.WriteLine(o2.Pop());
+
+        }
+    }
+    class IntegerStack<T> 
+        //where T : class       // T must be a reference type
+        //where T : struct      //T must be a value type
+        //where T : className   //T must be the class or its derived class
+        //where T : interfaceName // T must be a class that implements the inteface
+        //where T : new()           // T must have a no parameter consturctor
+        //where T : className,interfaceName,new()
+       
+    {
+        int[] arr;
+        int Pos = -1;
+
+        public IntegerStack(int size)
+        {
+            arr = new int[size];
+        }
+
+        public void Push(int i)
+        {
+            if (Pos == (arr.Length - 1))
+            {
+                throw new Exception("Stack full");
+            }
+
+            arr[++Pos] = i;
+        }
+
+        public int Pop()
+        {
+            if (Pos == -1)
+            {
+                throw new Exception("Stack Empty");
+            }
+
+            return arr[Pos--];
+        }
+    }
+
+    class StringStack
+    {
+        String[] arr;
+        int Pos = -1;
+
+        public StringStack(int size)
+        {
+            arr = new String[size];
+        }
+
+        public void Push(String i)
+        {
+            if (Pos == (arr.Length - 1))
+            {
+                throw new Exception("Stack full");
+            }
+
+            arr[++Pos] = i;
+        }
+
+        public String Pop()
+        {
+            if (Pos == -1)
+            {
+                throw new Exception("Stack Empty");
+            }
+
+            return arr[Pos--];
+        }
+    }
+
+    class MyStack<T>
+    {
+        T[] arr;
+        int Pos = -1;
+
+        public MyStack(int size)
+        {
+            arr = new T[size];
+        }
+
+        public void Push(T i)
+        {
+            if (Pos == (arr.Length - 1))
+            {
+                throw new Exception("Stack full");
+            }
+
+            arr[++Pos] = i;
+        }
+
+        public T Pop()
+        {
+            if (Pos == -1)
+            {
+                throw new Exception("Stack Empty");
+            }
+
+            return arr[Pos--];
+        }
+    }
+
+
+}
+
